96f4472e038590c662e8f1809543de8eeb6d779d
kfusilier <kristinfusilier@gmail.com>
2022-03-24 11:40:00 -0600

    admin login working, linked book model with db

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Book # import the Cat model from models.py
 # Register your models here.
+
+admin.site.register(Book) # this line will add the model to the admin panel
