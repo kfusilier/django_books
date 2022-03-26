3b531d4b0602d9079374817ced138d473a4fed7f
kfusilier <kristinfusilier@gmail.com>
2022-03-26 11:56:52 -0600

    changed model category

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY_CHOICES = (
-	("Fic", "Fiction"),
-	("NonFic", "Non-fiction")
+	("Fiction", "Fiction"),
+	("Non-Fiction", "Non-fiction")
 )
 
 # GENRE_CHOICES = (
@@ -17,7 +18,7 @@ class Book(models.Model):
 	author = models.CharField(max_length=50)
 	img = models.CharField(max_length=250)
 	published = models.IntegerField()
-	category = models.CharField(max_length=10, choices = CATEGORY_CHOICES)
+	category = models.CharField(max_length=50, choices = CATEGORY_CHOICES)
 	created_at = models.DateTimeField(auto_now_add=True)
 	
 	def __str__(self):
