2d159ceb35a487b38ac9ed7c53db9d7044851935
kfusilier <kristinfusilier@gmail.com>
2022-03-26 14:31:50 -0600

    modified model author and publisher to author1/publisher1 to start creating many to many

--- main_app/models.py
+++ main_app/models.py
@@ -10,10 +10,10 @@ CATEGORY_CHOICES = (
 class Book(models.Model):
 
 	title = models.CharField(max_length=100)
-	author = models.CharField(max_length=50)
+	author1 = models.CharField(max_length=50)
 	img = models.CharField(max_length=250)
 	published = models.IntegerField()
-	publisher = models.CharField(max_length=100)
+	publisher1 = models.CharField(max_length=100)
 	category = models.CharField(max_length=50, choices = CATEGORY_CHOICES) # 1-many
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	created_at = models.DateTimeField(auto_now_add=True)
