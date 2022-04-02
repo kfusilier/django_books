1aaf3200f80d9cec2a4ef80a7479ebdffd061e2b
kfusilier <kristinfusilier@gmail.com>
2022-04-02 00:05:56 -0600

    auth working

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 	path('books/<int:pk>/', views.Book_Detail.as_view(), name="book_detail"),
 	path('books/<int:pk>/update', views.Book_Update.as_view(), name="book_update"),
 	path('books/<int:pk>delete', views.Book_Delete.as_view(), name="book_delete"),
-	
+
 	path('user/<username>/', views.profile, name='profile'),
 
 	path('authors/', views.authors_index, name='authors_index'),
@@ -18,4 +18,7 @@ urlpatterns = [
 	path('authors/create/', views.Author_Create.as_view(), name='authors_create'),
 	path('authors/<int:pk>/update/', views.Author_Update.as_view(), name='authors_update'),
 	path('authors/<int:pk>/delete/', views.Author_Delete.as_view(), name='authors_delete'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,10 @@ from django.http import HttpResponseRedirect
 from django.views.generic import DetailView
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Book, Author #import Book model
 
@@ -33,6 +37,7 @@ class Book_List(TemplateView):
 			context["header"] = "Book List"
 		return context
 
+@method_decorator(login_required, name='dispatch')
 class Book_Create(CreateView):
 	model = Book
 	fields = ['title', 'authors', 'img', 'published', 'category', 'user']
@@ -50,6 +55,7 @@ class Book_Detail(DetailView):
 	model = Book
 	template_name="book_detail.html"
 
+@method_decorator(login_required, name='dispatch')
 class Book_Update(UpdateView):
 	model = Book
 	fields = ['title', 'authors', 'img', 'published', 'category', 'user']
@@ -57,12 +63,14 @@ class Book_Update(UpdateView):
 	def get_success_url(self):
 		return reverse('book_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required, name='dispatch')
 class Book_Delete(DeleteView):
 	model = Book
 	template_name = "book_delete_confirm.html"
 	success_url = "/books/"
 
 # profile for the user
+@login_required
 def profile(request, username):
     user = User.objects.get(username=username)
     books = Book.objects.filter(user=user)
@@ -77,19 +85,63 @@ def authors_show(request, author_id):
     author = Author.objects.get(id=author_id)
     return render(request, 'author_show.html', {'author': author})
 
+@method_decorator(login_required, name='dispatch')
 class Author_Create(CreateView):
     model = Author
     fields = '__all__'
     template_name = "author_form.html"
     success_url = '/authors'
 
+@method_decorator(login_required, name='dispatch')
 class Author_Update(UpdateView):
     model = Author
     fields = ['name']
     template_name = "author_update.html"
     success_url = '/authors'
 
+@method_decorator(login_required, name='dispatch')
 class Author_Delete(DeleteView):
     model = Author
     template_name = "author_confirm_delete.html"
     success_url = '/authors'
+
+# login, logout and signup
+def login_view(request):
+     # if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        # form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+            else:
+                print('The username and/or password is incorrect.')
+    else: # it was a get request so send the emtpy login form
+        # form = LoginForm()
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            print('HEY', user.username)
+            return HttpResponseRedirect('/user/'+str(user))
+        else:
+            HttpResponse('<h1>Try Again</h1>')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
