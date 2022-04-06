270af9685e51d12514caf2b4239c008a817762d7
kfusilier <kristinfusilier@gmail.com>
2022-04-05 19:23:24 -0600

    more styling, collections working

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Author Model
 class Author(models.Model):
 	name = models.CharField(max_length=100)
-
+	
 	def __str__(self):
 		return self.name
 
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,12 +5,14 @@ from . import views
 urlpatterns = [
 	path('', views.Home.as_view(), name='home'), # Added home path
 	path('books/', views.Book_List.as_view(), name='book_list'),
-	# path('about/', views.About.as_view(), name='about'),
+	# path('about/', views.Book_List.as_view(), name='about'),
 	path('books/new/', views.Book_Create.as_view(), name="book_create"),
 	path('books/<int:pk>/', views.Book_Detail.as_view(), name="book_detail"),
 	path('books/<int:pk>/update', views.Book_Update.as_view(), name="book_update"),
 	path('books/<int:pk>delete', views.Book_Delete.as_view(), name="book_delete"),
+
 	path('user/<username>/', views.profile, name='profile'),
+
 	path('authors/', views.authors_index, name='authors_index'),
 	path('authors/<int:author_id>', views.authors_show, name='authors_show'),
 	path('authors/create/', views.Author_Create.as_view(), name='authors_create'),
